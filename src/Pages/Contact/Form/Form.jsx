91c987aae5018103a994cd90e1c57c1b3999f95f
aram58641 <117660509+aram58641@users.noreply.github.com>
2023-02-22 20:20:11 +0400

    Update Form.jsx

--- src/Pages/Contact/Form/Form.jsx
+++ src/Pages/Contact/Form/Form.jsx
@@ -3,8 +3,11 @@
 
 function Form() {
     return(
+        <>
         <h1>Welcome Contact Page</h1>
-        <h1>alo</h1>
+        <h1>sadasdasdasdasdasdasd </h1>
+        </>
+        
     )
 }
 
