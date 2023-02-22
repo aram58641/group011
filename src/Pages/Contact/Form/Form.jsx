446ee1b922ae97604f8b49af269611729e094ccf
aram58641 <aramhakobyan844@gmail.com>
2023-02-22 20:22:08 +0400

    'asdasd'

--- src/Pages/Contact/Form/Form.jsx
+++ src/Pages/Contact/Form/Form.jsx
@@ -3,8 +3,11 @@
 
 function Form() {
     return(
-        <h1>Welcome Contact Page</h1>,
-        <h1>helo </h1>
+        <>
+           <h1>Welcome Contact Page</h1>
+        aasdasdasdasd
+        </>
+     
     )
 }
 
